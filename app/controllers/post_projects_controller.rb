f339f85c22bfa08af3f2f45d4fc2c5d5739099e0
dungtqa <dungtqa@gmail.com>
2010-02-01 17:44:29 +0700

    list project

--- app/controllers/post_projects_controller.rb
+++ app/controllers/post_projects_controller.rb
@@ -1,26 +1,20 @@
 # © Copyright 2009 AfterClassroom.com — All Rights Reserved
 class PostProjectsController < ApplicationController
   include Viewable
-  
-  before_filter :params_search_post, :only => [:index, :show, :search, :due_date, :edit]
-  before_filter :login_required, :except => [:index, :show]
-  before_filter :require_current_user,
-    :only => [:edit, :update, :destroy]
-  after_filter :store_location, :only => [:index]
+
+  before_filter :get_variables, :only => [:index, :show, :search, :due_date]
+  before_filter :login_required, :except => [:index, :show, :search, :due_date]
+  before_filter :require_current_user, :only => [:edit, :update, :destroy]
+  after_filter :store_location, :only => [:index, :show, :search, :due_date]
   # GET /post_projects
   # GET /post_projects.xml
   def index
-    @list_years = [["Chose year", ""], ["1st Year", "1year"], ["2nd Year", "2year"], ["3rd Year", "3year"], ["4th Year", "4year"], ["Ms.C", "ms.c"], ["Ph.D", "ph.d"]]
-    @list_overs = [["Over 30 days", "30"], ["Over 3 months", "90"], ["Over 6 months", "180"], ["Over 9 months", "270"], ["Over 1 year", "365"]]
-    @year = params[:year] if params[:year] 
-    @over = params[:over] if params[:over] 
-    type = PostCategory.find_by_name("Projects").id
     if params[:more_like_this_id]
-      post = Post.find_by_id(params[:more_like_this_id])
-      @posts = PostProject.paginated_post_more_like_this(post).paginate :page => params[:page], :per_page => 10
+      id = params[:more_like_this_id]
+      post = Post.find_by_id(id)
+      @posts = Post.paginated_post_more_like_this(params, post)
     else
-      school = session[:your_school]
-      @posts = Post.paginated_post_conditions_with_option(params, school, type)
+      @posts = Post.paginated_post_conditions_with_option(params, @school, @type)
     end
 
     respond_to do |format|
@@ -30,56 +24,36 @@ class PostProjectsController < ApplicationController
   end
 
   def search
-    @list_years = [["Chose year", ""], ["1st Year", "1year"], ["2nd Year", "2year"], ["3rd Year", "3year"], ["4th Year", "4year"], ["Ms.C", "ms.c"], ["Ph.D", "ph.d"]]
-    @list_overs = [["Over 30 days", "30"], ["Over 3 months", "90"], ["Over 6 months", "180"], ["Over 9 months", "270"], ["Over 1 year", "365"]]
-    @year = params[:year] if params[:year]
-    @over = params[:over] if params[:over]
-    type = PostProject.find_by_name("Projects").id
+    @query = params[:search][:query] if params[:search]
     if params[:search]
-      school = session[:your_school]
-      @posts = Post.paginated_post_conditions_with_search(params, school, type)
+      @posts = Post.paginated_post_conditions_with_search(params, @school, @type)
     end
-    
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @posts }
     end
   end
   
-    def due_date
-      @list_years = [["Chose year", ""], ["1st Year", "1year"], ["2nd Year", "2year"], ["3rd Year", "3year"], ["4th Year", "4year"], ["Ms.C", "ms.c"], ["Ph.D", "ph.d"]]
-      @list_overs = [["Over 30 days", "30"], ["Over 3 months", "90"], ["Over 6 months", "180"], ["Over 9 months", "270"], ["Over 1 year", "365"]]
-      @year = params[:year] if params[:year]
-      @over = params[:over] if params[:over]
-      type = PostCategory.find_by_name("Projects").id
-      school = session[:your_school]
-      @posts = PostProject.paginated_post_conditions_with_due_date(params, school, type)
-      
-      respond_to do |format|
-        format.html # index.html.erb
-        format.xml  { render :xml => @posts }
-      end
+  def due_date
+    @posts = PostProject.paginated_post_conditions_with_due_date(params, @school)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @posts }
     end
+  end
 
   # GET /post_projects/1
   # GET /post_projects/1.xml
   def show
-    @post_project = PostProject.find(params[:id])
-    @post = @post_project.post
-    @post_category_id = @post.post_category_id
-    @type_name = @post.post_category.name
-    @comments = @post.comments.find(:all, :limit => 5, :order => "created_at DESC")
-    update_views(@post_project.post)
+    @post = Post.find(params[:id])
+    @post_assignment = @post.post_assignment
+    update_view_count(@post)
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @post_project }
+      format.xml  { render :xml => @post_assignment }
     end
-  end
-
-  def show_dialog
-    @post = Post.find(params[:id])
-    update_views(@post)
-    render :layout => false
   end
 
   # GET /post_projects/new
@@ -145,19 +119,15 @@ class PostProjectsController < ApplicationController
     redirect_to my_post_user_url(current_user)
   end
 
-  def update_views(obj)
-    updated = update_view_count(obj)
-  end
-
   private
 
-def params_search_post
-    @query = params[:search][:query] if params[:search] 
+  def get_variables
+    @new_post_path = new_post_assignment_path
     @type = PostCategory.find_by_name("Projects").id
-    @search_post_path = post_projects_path
-    @new_post_path = new_post_project_path
-end
-  
+    @school = session[:your_school]
+    @query = params[:search][:query] if params[:search]
+  end
+
   def require_current_user
     @user ||= PostProject.find(params[:id]).post.user
     unless (@user && (@user.eql?(current_user)))
--- app/models/post_project.rb
+++ app/models/post_project.rb
@@ -2,16 +2,17 @@
 class PostProject < ActiveRecord::Base
   # Validations
   validates_presence_of :post_id
+
   # Relations
   belongs_to :post
 
+  # Named Scope
+  named_scope :due_date, lambda {|sc| return {:conditions => ["due_date > ?", Time.now], :order => "due_date DESC"} if sc.nil?; {:joins => :post, :conditions => ["school_id = ? AND due_date > ?", sc, Time.now], :order => "due_date DESC"}}
+  named_scope :with_limit, :limit => 5
 
-  def self.paginated_post_conditions_with_due_date(params, school, type)
-    cond = Caboose::EZ::Condition.new :post_projects do
-      due_date > Time.now
-    end
+  def self.paginated_post_conditions_with_due_date(params, school)
     posts = []
-    post_as = PostProject.find(:all, :conditions => cond.to_sql(), :order => "due_date DESC")
+    post_as = self.due_date(school)
     post_as.select {|p| posts << p.post}
     posts.paginate :page => params[:page], :per_page => 10
   end
