bfc14db0530c3e4f20a41f02d27eef26fe456030
datnt <ngothiendat@gmail.com>
2010-04-03 12:18:20 +0700

    add thick box to every change of setting tab, commit data to controller succesful

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,10 +6,14 @@ class SettingsController < ApplicationController
   end
 
   def setting
+    if params[:inf_msg]
+      @inf_msg =params[:inf_msg]
+    else
+      @inf_msg = ""
+    end
   end
 
   def networks
-    render :layout => false
   end
 
   def notifications
@@ -22,6 +26,64 @@ class SettingsController < ApplicationController
   end
   
   def ads
+  end
+
+  def change
+
+    if params[:typeOfChange] == "name"
+      @label = "Please enter new name"
+    elsif params[:typeOfChange] == "email"
+      @label = "Please enter new email address"
+    elsif params[:typeOfChange] == "psw"
+      @label = "Please enter new password"
+    end
+    render :layout => false
+  end
+
+  def savechange
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    puts "HHHHHHHHHHHHHHHHHHHHHHHH"
+    if params[:editType]
+      puts "=================="
+      puts "=================="
+      puts "=================="
+      puts "=================="
+      puts "=================="
+      puts "=================="
+      puts "=================="
+      puts "=================="
+      puts "=================="
+      puts "=================="
+      puts "=================="
+      puts "=================="+params[:editType]
+    end
+    redirect_to :action => "setting", :inf_msg => "Updated Successfully"
   end
 
 end
