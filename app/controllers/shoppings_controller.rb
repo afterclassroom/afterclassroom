a06a5f27d45efdcee32996458ed24c3964e323d9
datnt <ngothiendat@gmail.com>
2010-07-31 10:58:36 +0700

    thuc hien load tat cac selling_item thuoc ve friend cua current_user

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -5,6 +5,17 @@ class ShoppingsController < ApplicationController
 
   def mainpage
     @categories = Shoppingcategory.find(:all)
+
+    @friends_id = []
+		for friend in current_user.user_friends
+			@friends_id << friend.id
+		end
+    
+    @listallitems = SellingItem.friend_item(@friends_id)
+    #PHAN CONG VIEC TIEP THEO CAN LAM DOI VOI MUC NAY LA`:
+    #1> DA LO.C DUOC CAC SELLING_ITEM THUOC VE FRIEND CUA CURRENT_USER
+    #2> CAN LO.C LAI 1 LAN NUA DE NHAN DUOC CAC SELLING_ITEM THUOC VE SELECTED_SUB_CATEGORY
+
     puts params[:sub_category_name]
     puts params[:sub_category_name]
     puts params[:sub_category_name]
