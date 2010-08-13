8abd8fd644e17a916bda5edad2d2576f56efa515
dungtqa <dungtqa@gmail.com>
2010-08-13 15:25:11 +0700

    Fix multi upload photo

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -21,7 +21,7 @@ module PhotosHelper
       function_send_form_data=<<-EOS
       function uploadStart(){
         //add fom parameters
-        var datas = $("#{form_id}").serializeArray();
+        var datas = $("##{form_id}").serializeArray();
         for (var i = 0; i < datas.length; i++) {
          swfu.addPostParam(datas[i].name,datas[i].value);
         }
@@ -31,56 +31,57 @@ module PhotosHelper
 
     return_data=""
     return_data+=<<-EOS
-  <script type="text/javascript">
-    var swfu;
-    var isIE = false;
-    window.onload = function() {
-      var settings = {
-        flash_url : "/javascripts/swf/swfupload.swf",
-        upload_url: "#{upload_url}?_geokit_session_id=#{session.session_id}",
-        file_post_name : "#{file_post_name}",
-        post_params : {"#{request_forgery_protection_token}":"#{escape_javascript form_authenticity_token}"},
-        file_size_limit : "#{file_size_limit}",
-        file_types : "#{file_types}",
-        file_types_description : "All Files",
-        file_upload_limit : #{file_upload_limit},
-        file_queue_limit : 0,
-        custom_settings : {
-          progressTarget : "fsUploadProgress",
-          cancelButtonId : "btnCancel"
-        },
-        debug: #{debug},
+  <script type='text/javascript'>
+  var swfu;
 
-        // Button settings
-        button_image_url: "#{button_image_url}",
-        button_width: "65",
-        button_height: "29",
-        button_placeholder_id: "spanButtonPlaceHolder",
-        button_text: '<span class="theFont">Upload</span>',
-        button_text_style: ".theFont { font-size: 16; }",
-        button_text_left_padding: 12,
-        button_text_top_padding: 3,
+  window.onload = function () {
+    swfu = new SWFUpload({
+      upload_url: "#{upload_url}?_geokit_session_id=#{session.session_id}",
+      flash_url : "/flash/swfupload.swf",
 
-        // The event handler functions are defined in handlers.js
-        file_queued_handler : fileQueued,
-        file_queue_error_handler : fileQueueError,
-        file_dialog_complete_handler : fileDialogComplete,
-        upload_start_handler : uploadStart,
-        upload_progress_handler : uploadProgress,
-        upload_error_handler : uploadError,
-        upload_success_handler : uploadSuccess,
-        upload_complete_handler : uploadComplete,
-        queue_complete_handler : queueComplete	// Queue plugin event
-      };
+      // Button settings
+      button_image_url: "#{button_image_url}",
+      button_width: "65",
+      button_height: "29",
+      button_placeholder_id: "spanButtonPlaceHolder",
+      button_text: '<span class="theFont">Upload</span>',
+      button_text_style: ".theFont { font-size: 16; }",
+      button_text_left_padding: 12,
+      button_text_top_padding: 3,
 
-      swfu = new SWFUpload(settings);
-    };
+      file_post_name : "#{file_post_name}",
+      file_size_limit : "#{file_size_limit}",
+      file_types : "#{file_types}",
+      file_types_description : "All Files",
+      file_upload_limit : #{file_upload_limit},
+      file_queue_limit : 0,
 
-    function uploadSuccess(fileObj,data) {
-      eval(data);
-    }
-    #{function_send_form_data}
-  </script>
+      custom_settings : {
+        progressTarget : "fsUploadProgress"
+      },
+
+      file_queue_error_handler : fileQueueError,
+      file_dialog_complete_handler : fileDialogComplete,
+      upload_start_handler : uploadStart,
+      upload_progress_handler : uploadProgress,
+      upload_error_handler : uploadError,
+      upload_success_handler : uploadSuccess,
+      upload_complete_handler : uploadComplete,
+
+      post_params : {
+        authenticity_token : "#{escape_javascript form_authenticity_token}",
+      },
+
+      custom_settings : {
+        upload_target : 'divFileProgressContainer',
+      },
+
+      debug: #{debug},
+
+    });
+  };
+  #{function_send_form_data}
+</script>
     EOS
     return_data
   end
