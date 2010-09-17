a66fd0b3afc6a3fb33ae1aee0295a4ccc4c09db3
dungtqa <dungtqa@gmail.com>
2010-09-17 14:31:38 +0700

    Start server

--- config/deploy.rb
+++ config/deploy.rb
@@ -84,9 +84,17 @@ namespace :deploy do
   end
 
   namespace :deploy do
-    task :restart do
-      restart_mongrel_cluster
+    task :start, :roles => :app do
+      run "touch #{current_release}/tmp/restart.txt"
+    end
+
+    task :stop, :roles => :app do
+      # Do nothing.
+    end
+
+    desc "Restart Application"
+    task :restart, :roles => :app do
+      run "touch #{current_release}/tmp/restart.txt"
     end
   end
-
 end
