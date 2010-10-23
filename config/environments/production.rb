5103a651ca2546b2b0d4b8af72ef9da89f865dc7
Tran Quoc Anh Dung <dungtqa@dungtqa-laptop.(none)>
2010-10-23 20:58:30 +0700

    Fix s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,4 +22,6 @@ config.action_mailer.raise_delivery_errors = false
 REST_AUTH_SITE_KEY = '5a5e73a69a893311f859ccff1ffd0fa2d7ea25fd'
 REST_AUTH_DIGEST_STRETCHES = 15
 PAPERCLIP_STORAGE_OPTIONS = {:storage => :s3,
-  :s3_credentials => "#{Rails.root}/config/s3.yml"}
+  :s3_credentials => "#{Rails.root}/config/s3.yml",
+  :path => "/:style/:filename"
+  }
