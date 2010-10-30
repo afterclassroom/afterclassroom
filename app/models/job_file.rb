94460b259fe3c7a52ab3c070c5abb317d0bf1114
datnt <ngothiendat@gmail.com>
2010-10-30 11:22:10 +0700

    modify the button apply now

--- app/models/job_file.rb
+++ app/models/job_file.rb
@@ -3,9 +3,9 @@ class JobFile < ActiveRecord::Base
   
   has_attached_file :resume_cv, :default_url => ""
   
-  named_scope :with_current_user, lambda { |usr_id| {:conditions => ["job_files.user_id = ?", usr_id]} }
-  named_scope :with_letter, :conditions => { :category =>["Cover Letter"] }
-  named_scope :with_resume, :conditions => { :category =>["Resume"] }
-  named_scope :with_transcript, :conditions => { :category =>["Transcript"] }
+#  named_scope :with_current_user, lambda { |usr_id| {:conditions => ["job_files.user_id = ?", usr_id]} }
+#  named_scope :with_letter, :conditions => { :category =>["Cover Letter"] }
+#  named_scope :with_resume, :conditions => { :category =>["Resume"] }
+#  named_scope :with_transcript, :conditions => { :category =>["Transcript"] }
   
 end
