968f5c09eb268537362797e7e9ad455326e2fa36
linkdigital <cms3@ubuntu.(none)>
2011-03-28 14:48:56 +0700

    fix error youtube

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:create]
   protect_from_forgery :only => [:create]
   
-  before_filter RubyCAS::Filter::GatewayFilter
+  before_filter RubyCAS::Filter::GatewayFilter, :except => [:create]
   before_filter RubyCAS::Filter, :except => [:new, :show, :create, :activate, :forgot_login, :forgot_password]
   before_filter :cas_user
   #before_filter :login_required, :except => [:new, :show, :create, :activate, :forgot_login, :forgot_password]
