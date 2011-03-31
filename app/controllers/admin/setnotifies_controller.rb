755002bc5ac029dc6110554fed7ebce1a658a69e
Ngo Thien Dat <datnt@datnt-ThinkPad-T61.(none)>
2011-03-31 14:21:14 +0700

    successfully create a new notification

--- app/controllers/admin/setnotifies_controller.rb
+++ app/controllers/admin/setnotifies_controller.rb
@@ -5,6 +5,7 @@ class Admin::SetnotifiesController < ApplicationController
 
   def index
     @notifications = Notification.find(:all, :order => "notify_type")
+    @notify_message = ""
   end
 
   def addnew
@@ -35,6 +36,33 @@ class Admin::SetnotifiesController < ApplicationController
     puts "VALUE MOBILE ==  "+params[:email].to_s
     puts "VALUE combobox ==  "+params[:list_cate].to_s
     puts "VALUE name ==  "+params[:name].to_s
+
+    @notification = Notification.new
+    @notification.name = params[:name].to_s
+
+    if params[:email].to_s == "on"
+      @notification.email_allow = true
+    else
+      @notification.email_allow = false
+    end
+    if params[:sms].to_s == "on"
+      @notification.sms_allow = true
+    else
+      @notification.sms_allow = false
+    end
+     
+    @notification.notify_type = params[:list_cate].to_s
+
+    @notification.label = @notification.name.downcase
+    @notification.label.gsub!(" ", "_")
+
+    
+
+    if @notification.save
+      @notify_message = "Success create new Notification"
+    else
+      @notify_message = "Failed to create new Notification"
+    end
 
     redirect_to :action => "index"
   end
