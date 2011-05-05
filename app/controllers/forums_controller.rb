8bf33689457b488d2693afc13d1dac0a5d17282e
datnt <datnt@datnt-laptop.(none)>
2011-05-05 11:58:24 +0700

    display comment

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -18,14 +18,27 @@ class ForumsController < ApplicationController
     puts "heLLO WORLD"
     puts "heLLO WORLD"
     puts "heLLO WORLD"
-    puts "heLLO WORLD"
+    puts "heLLO WORLD forum_id == "+params[:forum_id]
+
+    current_fr = Forum.find(params[:forum_id])
 
     comment = params[:comment]
     commentable_id = params[:commentable_id]
     commentable_type = params[:commentable_type]
+
+    #can truyen forum id len theo
  
 
-    puts "heLLO WORLD"
+    @obj_comment = Comment.new()
+    @obj_comment.comment = params[:content]
+    @obj_comment.user = current_user
+    @obj_comment.save
+
+    current_fr.comments << @obj_comment
+
+
+    puts "after save comment :::: "+current_fr.comments.length.to_s
+
     render :text => "text from save_cmt "+params[:content]
   end
 
