4f3ee6507f5bb6f4327f931b64fa0af8d23d105e
Tran Quoc Anh Dung <dungtqa@dungtqa.(none)>
2011-05-09 23:07:58 +0700

    Fix error

--- app/models/post.rb
+++ app/models/post.rb
@@ -49,8 +49,8 @@ class Post < ActiveRecord::Base
     indexes title, :sortable => true
     indexes description
     has user_id, post_category_id, school_id, created_at
-    #set_property :delta => :delayed
-    set_property :delta => true
+    set_property :delta => :delayed
+    #set_property :delta => true
   end
   
   def self.paginated_post_conditions_with_search(params, school, type)
--- app/models/post_exam_schedule.rb
+++ app/models/post_exam_schedule.rb
@@ -25,7 +25,7 @@ class PostExamSchedule < ActiveRecord::Base
     with_school = from_school if from_school
 
     post_ts = PostExamSchedule.ez_find(:all, :include => [:post], :order => "posts.created_at DESC") do |post_exam, post|
-      post_exam.type = type
+      post_exam.type_name == type
       post.department_id == department if department
       post.school_year == year if year
       post.school_id == with_school if with_school
@@ -39,7 +39,7 @@ class PostExamSchedule < ActiveRecord::Base
 
   def self.paginated_post_more_like_this(params, post_like)
     post_ts = PostExamSchedule.ez_find(:all, :include => [:post]) do |post_exam, post|
-      post_exam.type = post_like.post_exam_schedule.type
+      post_exam.type_name = post_like.post_exam_schedule.type
       post.department_id == post_like.department_id
       post.school_year == post_like.school_year
       post.school_id == post_like.school_id
