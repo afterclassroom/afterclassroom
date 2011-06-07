68643967ba0123e3c389b1de585524f02ffc9216
Huynh Bao Viet <baoviet@goodluck.vn>
2011-06-07 12:03:09 +0700

    no bug 187

--- public/javascripts/scroll-startstop.js
+++ public/javascripts/scroll-startstop.js
@@ -20,50 +20,15 @@
                         jQuery.event.handle.apply(_self, _args);
                     }
                     
-                    timer = setTimeout( function(){
-                        timer = null;
-                    }, special.scrollstop.latency);
-                    
-                };
-            
-            jQuery(this).bind('scroll', handler).data(uid1, handler);
-            
-        },
-        teardown: function(){
-            jQuery(this).unbind( 'scroll', jQuery(this).data(uid1) );
-        }
-    };
-    
-    special.scrollstop = {
-        latency: 300,
-        setup: function() {
-            
-            var timer,
-                    handler = function(evt) {
-                    
-                    var _self = this,
-                        _args = arguments;
-                    
-                    if (timer) {
-                        clearTimeout(timer);
-                    }
-                    
-                    timer = setTimeout( function(){
-                        
-                        timer = null;
-                        evt.type = 'scrollstop';
-                        jQuery.event.handle.apply(_self, _args);
-                        
-                    }, special.scrollstop.latency);
+                  
                     
                 };
             
             jQuery(this).bind('scroll', handler).data(uid2, handler);
             
         },
-        teardown: function() {
-            jQuery(this).unbind( 'scroll', jQuery(this).data(uid2) );
-        }
+       
     };
     
+   
 })();
