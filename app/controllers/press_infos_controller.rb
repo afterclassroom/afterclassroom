5667d629a855f0c7ebf554d39c97642af24926bd
Ngo Thien Dat <datnt@datnt-ThinkPad-T61.(none)>
2011-07-08 14:57:27 +0700

    create look and feel for pressRelease

--- app/controllers/press_infos_controller.rb
+++ app/controllers/press_infos_controller.rb
@@ -1,5 +1,5 @@
 class PressInfosController < ApplicationController
   def index
-    
+    @presses = PressInfo.find(:all, :order => "created_at DESC").paginate(:page => params[:page], :per_page => 10, :order => "created_at")
   end
 end
