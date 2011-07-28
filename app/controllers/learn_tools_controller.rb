7f5ebfc1f5373329b5c5d5b873196f77d2eea74f
ubuntu11 <datnt@ubuntu.(none)>
2011-07-28 17:15:23 +0700

    cate select

--- app/controllers/learn_tools_controller.rb
+++ app/controllers/learn_tools_controller.rb
@@ -6,6 +6,8 @@ class LearnToolsController < ApplicationController
     params[:feature_page] = params[:feature_page] ? params[:feature_page]  : "1"
     @str_feature_page = "Page #{params[:feature_page]} of "+ ( Learntool.with_featured.size / 2.0 ).round.to_s
     @tool_cats = LearnToolCate.find(:all)
+    
+    
   end
   
   def mylearn
@@ -20,4 +22,5 @@ class LearnToolsController < ApplicationController
     
     render :layout => false
   end
+  
 end
