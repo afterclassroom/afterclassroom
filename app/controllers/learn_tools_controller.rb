cab83c2ae3306c935270865f346e970084021038
ubuntu11 <datnt@ubuntu.(none)>
2011-07-28 14:49:53 +0700

    load tool cate

--- app/controllers/learn_tools_controller.rb
+++ app/controllers/learn_tools_controller.rb
@@ -5,6 +5,7 @@ class LearnToolsController < ApplicationController
     @features = Learntool.paging_featured(params)
     params[:feature_page] = "1"
     @str_feature_page = "Page #{params[:feature_page]} of "+ ( Learntool.with_featured.size / 2 ).round.to_s
+    @tool_cats = LearnToolCate.find(:all)
   end
   
   def mylearn
