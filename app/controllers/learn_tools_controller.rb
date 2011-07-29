c143f071bb7e837143ca56d40414d74cbf514e36
ubuntu11 <datnt@ubuntu.(none)>
2011-07-29 11:55:22 +0700

    paging with tool cate

--- app/controllers/learn_tools_controller.rb
+++ app/controllers/learn_tools_controller.rb
@@ -4,14 +4,23 @@ class LearnToolsController < ApplicationController
   before_filter :get_variables, :only => [:index, :search_tool]
   
   def index
-    @features = Learntool.paging_featured(params)
-    params[:feature_page] = params[:feature_page] ? params[:feature_page]  : "1"
-    @str_feature_page = "Page #{params[:feature_page]} of "+ ( Learntool.with_featured.size / 2.0 ).round.to_s
+    if params[:tool_cate] == "-1"
+      @features = Learntool.paging_featured(params)
+      params[:feature_page] = params[:feature_page] ? params[:feature_page]  : "1"
+      @str_feature_page = "Page #{params[:feature_page]} of "+ ( Learntool.with_featured.size / 2.0 ).round.to_s
     
-    
-    params[:may_like_page] = params[:may_like_page] ? params[:may_like_page]  : "1"
-    @maylikes = Learntool.paging_may_like(params)
-    @str_maylike_page = "Page #{params[:may_like_page]} of "+ ( Learntool.with_may_like.size / 10.0 ).round.to_s
+      params[:may_like_page] = params[:may_like_page] ? params[:may_like_page]  : "1"
+      @maylikes = Learntool.paging_may_like(params)
+      @str_maylike_page = "Page #{params[:may_like_page]} of "+ ( Learntool.with_may_like.size / 10.0 ).round.to_s
+    else #when user filter tool by category
+      #with_cate
+      @features = Learntool.with_cate(params[:tool_cate]).paging_featured(params)
+      @maylikes = Learntool.with_cate(params[:tool_cate]).paging_may_like(params)
+      params[:feature_page] = "1"
+      params[:may_like_page]= "1"
+      @str_feature_page = "Page #{params[:feature_page]} of "+ ( Learntool.with_cate(params[:tool_cate]).with_featured.size / 2.0 ).round.to_s
+      @str_maylike_page = "Page #{params[:may_like_page]} of "+ ( Learntool.with_cate(params[:tool_cate]).with_may_like.size / 10.0 ).round.to_s
+    end
   end
   
   def mylearn
@@ -19,9 +28,18 @@ class LearnToolsController < ApplicationController
   end
   
   def featured_tool_paging
-    params[:feature_page] = params[:page_to_load]
-    @features = Learntool.paging_featured(params)
-    @str_feature_page = "Page #{params[:feature_page]} of "+ ( Learntool.with_featured.size / 2 ).round.to_s
+    if params[:cur_cate_at_feature] == "-1"#there is no category selected
+      params[:feature_page] = params[:page_to_load]
+    
+      @features = Learntool.paging_featured(params)
+      @str_feature_page = "Page #{params[:feature_page]} of "+ ( Learntool.with_featured.size / 2.0 ).round.to_s
+    else
+      params[:feature_page] = params[:page_to_load]
+    
+      @features = Learntool.with_cate(params[:cur_cate_at_feature]).paging_featured(params)
+      @str_feature_page = "Page #{params[:feature_page]} of "+ ( Learntool.with_cate(params[:cur_cate_at_feature]).with_featured.size / 2.0 ).round.to_s
+      @cur_cate_at_feat = params[:cur_cate_at_feature]
+    end
     
     render :layout => false
   end
@@ -48,7 +66,7 @@ class LearnToolsController < ApplicationController
   
   def get_variables
     @tool_cats = LearnToolCate.find(:all)
-    params[:tool_cate] = params[:tool_cate] ? params[:tool_cate] : "Please select..."
+    params[:tool_cate] = params[:tool_cate] ? params[:tool_cate] : "-1"
   end
   
 end
