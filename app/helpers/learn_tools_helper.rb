349856a37eb41788589b4ce0a3be1832666fcaea
ubuntu11 <datnt@ubuntu.(none)>
2011-07-30 11:06:01 +0700

    update

--- app/helpers/learn_tools_helper.rb
+++ app/helpers/learn_tools_helper.rb
@@ -1,2 +1,11 @@
 module LearnToolsHelper
+  def show_friend_player (tool)
+    @count = 0
+    tool.my_tools.each do |mt|
+      if current_user.user_friends.include?(mt.user)
+        @count = @count + 1
+      end
+    end
+    @count
+  end
 end
