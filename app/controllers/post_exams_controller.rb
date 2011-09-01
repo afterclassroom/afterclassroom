61c372b5af197e7142cab09e0a6c24de3755d711
ubuntu11 <datnt@ubuntu.(none)>
2011-09-01 15:48:36 +0700

    interesting for exam

--- app/controllers/post_exams_controller.rb
+++ app/controllers/post_exams_controller.rb
@@ -43,7 +43,10 @@ class PostExamsController < ApplicationController
   end
   
   def interesting
-    @post_results = PostExam.paginated_post_conditions_with_interesting(params, @school)
+    #@post_results = PostExam.paginated_post_conditions_with_interesting(params, @school)
+    
+    @post_results = PostExam.recent_interesting(@school)
+    
     @posts = @post_results.paginate({:page => params[:page], :per_page => 10})
     respond_to do |format|
       format.html # index.html.erb
