2ddfd417fc8651029f5bb3f9e189bf618d0675ef
linkdigital <cms3@ubuntu.(none)>
2011-09-05 16:34:17 +0700

    Backup

--- config/backup.rb
+++ config/backup.rb
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-##
-=======
-#
->>>>>>> 206aed35fb467b11561905290ae32fd929ba0aaf
 # Backup
 # Generated Template
 #
@@ -17,18 +12,6 @@
 #
 # $ backup -t my_backup [-c <path_to_configuration_file>]
 
-<<<<<<< HEAD
-Backup::Model.new(:after_backup, 'After Backup') do
-
-  database MySQL do |db|
-    db.name               = "afterclassroom"
-    db.username           = "root"
-    #db.password           = "Ia9auNb6u*"
-    db.password           = ""
-    db.host               = "localhost"
-    db.port               = 3306
-    db.socket             = "/var/run/mysqld/mysqld.sock"
-=======
 Backup::Model.new(:db_backup, 'Database Backup to S3') do
 
   database MySQL do |db|
@@ -37,7 +20,6 @@ Backup::Model.new(:db_backup, 'Database Backup to S3') do
     db.password           = "9Jke.w9itA"
     db.host               = "localhost"
     db.port               = 3306
->>>>>>> 206aed35fb467b11561905290ae32fd929ba0aaf
     db.additional_options = ['--quick', '--single-transaction']
   end
 
@@ -45,13 +27,8 @@ Backup::Model.new(:db_backup, 'Database Backup to S3') do
     s3.access_key_id      = 'AKIAJ3B6N3Y22A2R2LZQ'
     s3.secret_access_key  = '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'
     s3.region             = 'us-east-1'
-<<<<<<< HEAD
-    s3.bucket             = 'backup_sql'
-    s3.path               = '/afterclassroom_backup'
-=======
     s3.bucket             = 'computer_backup'
     s3.path               = 'afterclassroom_database_backup'
->>>>>>> 206aed35fb467b11561905290ae32fd929ba0aaf
     s3.keep               = 10
   end
 
@@ -59,12 +36,9 @@ Backup::Model.new(:db_backup, 'Database Backup to S3') do
     compression.best = true
     compression.fast = false
   end
-<<<<<<< HEAD
 
 end
 
-=======
-	
 	notify_by Mail do |mail|
 		mail.on_success           = true
 		mail.on_failure           = true
@@ -80,4 +54,3 @@ end
 		mail.enable_starttls_auto = true
 	end
 end
->>>>>>> 206aed35fb467b11561905290ae32fd929ba0aaf
