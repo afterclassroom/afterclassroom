775892dc9c8f75becfb40f1f5d90d364e3ad092d
ubuntu11 <datnt@ubuntu.(none)>
2011-09-06 16:25:00 +0700

    update db

--- db/migrate/20110906091954_create_tag_infos.rb
+++ db/migrate/20110906091954_create_tag_infos.rb
@@ -2,7 +2,10 @@ class CreateTagInfos < ActiveRecord::Migration
   def self.up
     create_table :tag_infos do |t|
       t.integer :tag_creator_id
-      t.integer :tagable_id
+      t.integer :tagable_id #id of the Photo/Video to be tagged
+      t.string :tagable_type
+      t.integer :tagable_user
+      t.boolean :verify
 
       t.timestamps
     end
