3f822ed1c7c035a99ad29c7d042edda71e0742cf
Bao Viet <vietbeto@gmail.com>
2011-09-07 09:48:36 +0700

    style admin users

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Comment, :as => "PostComment" do
-  
+  filter :user
+  filter :comment
+  filter :created_at
+  filter :commentable_type
+  index do
+    column :comment
+		column :created_at
+		column :commentable_type
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register User do
-  
+  filter :school_id
+  filter :name
+  filter :email
+  filter :state
+  filter :created_at
+  filter :online
+  index do
+    column :name
+		column :email
+		column :state
+    column :created_at
+		column :school_id
+		column :online
+  end
 end
