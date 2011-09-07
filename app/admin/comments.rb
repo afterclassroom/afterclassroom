2eb766cdd2df5e8ac1f508e0e43b7f229449a5ce
Bao Viet <vietbeto@gmail.com>
2011-09-07 14:19:25 +0700

    style admin post_comments

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -4,8 +4,12 @@ ActiveAdmin.register Comment, :as => "PostComment" do
   filter :created_at
   filter :commentable_type
   index do
+    column "User" do |post|
+      post.user.name
+    end
     column :comment
 		column :created_at
 		column :commentable_type
+    default_actions
   end
 end
--- app/admin/report_abuses.rb
+++ app/admin/report_abuses.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register ReportAbuse do
   menu :parent => "Report Abuse"
+  filter :report_abuse_category
+  filter :reported_type
+  filter :created_at
+  filter :updated_at
+  index do
+    column "User" do |post|
+      User.find(post.reporter_id).name
+    end
+    column :reported_type
+		column :created_at
+		column :updated_at
+    default_actions
+  end
 end
