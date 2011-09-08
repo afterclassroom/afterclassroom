8d92dbbe1d988dc6ea7d42275a1b0e1b784af646
ubuntu11 <datnt@ubuntu.(none)>
2011-09-08 11:18:29 +0700

    apply function remove

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -230,6 +230,11 @@ class VideosController < ApplicationController
     redirect_to :controller=>'videos', :action => 'show', :id => params[:video_id]
   end
   
+  def remove_tagged
+    TagInfo.refuse(params[:tag_checkbox],params[:video_id])
+    redirect_to :controller=>'videos', :action => 'show', :id => params[:video_id]
+  end
+  
   protected
   
   def require_current_user
