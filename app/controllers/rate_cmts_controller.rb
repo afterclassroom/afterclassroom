5191ae0d4c5bb7ccfd8907fdb59ab939b42ccfa7
ubuntu11 <datnt@ubuntu.(none)>
2011-10-13 15:05:07 +0700

    add new cmt

--- app/controllers/rate_cmts_controller.rb
+++ app/controllers/rate_cmts_controller.rb
@@ -4,6 +4,27 @@ class RateCmtsController < ApplicationController
     render :layout => false
   end
   def add_like_cmt
+    @post_id = params[:post_id]
+    puts "=="
+    puts "== v"
+    puts "== #{params[:post_id]}"
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
+    puts "=="
     render :layout => false
   end
 end
