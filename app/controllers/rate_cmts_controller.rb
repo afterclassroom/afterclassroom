20a2307f4a2cd19d10f61f213638cec3b7ba024c
ubuntu11 <datnt@ubuntu.(none)>
2011-10-13 16:54:49 +0700

    save cmt to server

--- app/controllers/rate_cmts_controller.rb
+++ app/controllers/rate_cmts_controller.rb
@@ -5,11 +5,18 @@ class RateCmtsController < ApplicationController
   end
   def add_like_cmt
     @post_id = params[:post_id]
+    @cmt = RatingText.new()
+    @cmt.rater_id = current_user.id
+    @cmt.rated_id = params[:post_id]
+    @cmt.rated_type = "PostBook"
+    @cmt.rating = 1
+    @cmt.content = params[:cmt_cnt]
+    @cmt.save
     puts "=="
     puts "== v"
     puts "== #{params[:post_id]}"
-    puts "=="
-    puts "=="
+    puts "== content"
+    puts "== a == #{params[:cmt_cnt]}"
     puts "=="
     puts "=="
     puts "=="
