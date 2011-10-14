d99554f7ea35f4c1e1e464929c8cbf4ae0b53e84
ubuntu11 <datnt@ubuntu.(none)>
2011-10-14 10:44:02 +0700

    paging

--- app/controllers/rate_cmts_controller.rb
+++ app/controllers/rate_cmts_controller.rb
@@ -1,7 +1,7 @@
 class RateCmtsController < ApplicationController
   def load_bk_like
     @post = Post.find(params[:post_id])
-    @rating_texts = @post.rating_texts.order('created_at DESC')
+    @rating_texts = @post.rating_texts.order('created_at DESC').paginate(:page => 1, :per_page => 5)
     render :layout => false
   end
   def add_like_cmt
