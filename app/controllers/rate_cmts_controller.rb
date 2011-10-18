56d3a76fd45cbff64cd82233482a07c6c7a1cf24
ubuntu11 <datnt@ubuntu.(none)>
2011-10-18 14:32:12 +0700

    rate for post event

--- app/controllers/rate_cmts_controller.rb
+++ app/controllers/rate_cmts_controller.rb
@@ -5,6 +5,7 @@ class RateCmtsController < ApplicationController
     @rating_texts = @post.rating_texts.where(["rating=? and rated_type=?","1","#{params[:rated_type]}"]).order('created_at DESC').paginate(:page => @str_cur_page, :per_page => params[:str_perpage])
     @perpage = params[:str_perpage]
     @like_size = @post.rating_texts.where('rating="1"').size
+    @rated_type = params[:rated_type]
     render :layout => false
   end
   def add_like_cmt
@@ -25,7 +26,7 @@ class RateCmtsController < ApplicationController
     @str_cur_page = params[:str_page_no] ? params[:str_page_no] : "1"
     @rating_texts = @post.rating_texts.where(["rating=? and rated_type=?","0","#{params[:rated_type]}"]).order('created_at DESC').paginate(:page => @str_cur_page, :per_page => params[:str_perpage])
     @dislike_size = @post.rating_texts.where('rating = "0"').size
-
+    @rated_type = params[:rated_type]
     @perpage = params[:str_perpage]
     render :layout => false
   end
