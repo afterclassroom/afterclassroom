e68fcf97b341194d9acf568f7ccf7bca50f5614e
ubuntu11 <datnt@ubuntu.(none)>
2011-10-27 10:14:21 +0700

    update

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -321,8 +321,8 @@ class VideosController < ApplicationController
   def rate
     rating = params[:rating]
     @post = Video.find(params[:post_id])
-    # @post.rate rating.to_i, current_user
-    # @post.save
+    @post.rate rating.to_i, current_user
+    @post.save
     
     @text = "<div class='qashdU'><a href='javascript:;' class='vtip' title='#{configatron.str_rated}'>#{@post.total_good}</a></div>"
     @text << "<div class='qashdD'><a href='javascript:;' class='vtip' title='#{configatron.str_rated}'>#{@post.total_bad}</a></div>"
