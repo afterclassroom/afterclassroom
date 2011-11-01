fc61d989c338c484f84491facabe24321063e114
linkdigital <cms3@ubuntu.(none)>
2011-11-01 10:28:58 +0700

    Video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -337,6 +337,14 @@ class VideosController < ApplicationController
     @text = "<div class='qashdU'><a href='javascript:;' class='vtip' title='#{configatron.str_rated}'>#{@post.total_good}</a></div>"
     @text << "<div class='qashdD'><a href='javascript:;' class='vtip' title='#{configatron.str_rated}'>#{@post.total_bad}</a></div>"
   end
+
+	def get_on_this_video
+		video_id = params[:video_id]
+		@video = Video.find(video_id)
+		#list of user has been tagged, and been verified
+      @tagged_users = User.find(:all, :joins => "INNER JOIN tag_infos ON tag_infos.tagable_user = users.id", :conditions => ["tag_infos.tagable_id=? and tag_infos.tagable_type=? and tag_infos.verify=?",video_id,"Video",true ] )
+      @verify_users = User.find(:all, :joins => "INNER JOIN tag_infos ON tag_infos.tagable_user = users.id", :conditions => ["tag_infos.tagable_id=? and tag_infos.tagable_type=? and tag_infos.verify=?",video_id,"Video",false ] )
+	end
   
   protected
   
