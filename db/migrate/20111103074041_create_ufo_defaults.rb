368557557ffdd172769468266f122e7aad14ed5c
ubuntu11 <datnt@ubuntu.(none)>
2011-11-03 16:31:30 +0700

    update migrate setting

--- db/migrate/20111103074041_create_ufo_defaults.rb
+++ db/migrate/20111103074041_create_ufo_defaults.rb
@@ -2,7 +2,8 @@ class CreateUfoDefaults < ActiveRecord::Migration
   def self.up
     create_table :ufo_defaults do |t|
       t.belongs_to :user, :null => false
-      type_of_share
+      t.string :type_setting
+      t.boolean :post_lounge
 
       t.timestamps
     end
