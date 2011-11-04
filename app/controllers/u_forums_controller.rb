89c856ef222ec728a5a76339c8ae8ea640a3e82f
Ngo Thien Dat <datnt@datnt-ThinkPad-T61.(none)>
2011-11-04 15:24:42 +0700

    item for ufo

--- app/controllers/u_forums_controller.rb
+++ app/controllers/u_forums_controller.rb
@@ -36,32 +36,13 @@ class UForumsController < ApplicationController
     default_setting = UfoDefault.find_or_create_by_user_id(current_user.id)
     default_setting.share_to_index = params[:shareto]
     default_setting.post_lounge = params[:postlounge]
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == "
-    puts "v == 1"
-    puts "v == #{params[:shareto]}"
     default_setting.save
 
     render :layout => false 
+  end
+
+  def index
+    @ufos = current_user.ufos
   end
 
 end
