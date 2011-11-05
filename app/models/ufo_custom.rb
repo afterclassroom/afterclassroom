fec448203fc91f8cbcf8761aa532affbf5a18c57
ubuntu11 <datnt@ubuntu.(none)>
2011-11-05 08:54:20 +0700

    create custom setting

--- app/models/ufo_custom.rb
+++ app/models/ufo_custom.rb
@@ -1,2 +1,3 @@
 class UfoCustom < ActiveRecord::Base
+  belongs_to :ufo
 end
--- db/migrate/20111105015008_create_ufo_customs.rb
+++ db/migrate/20111105015008_create_ufo_customs.rb
@@ -1,7 +1,10 @@
 class CreateUfoCustoms < ActiveRecord::Migration
   def self.up
     create_table :ufo_customs do |t|
-      t.string :title
+      t.belongs_to :ufo, :null => false
+      t.string :share_to_index
+      #refer to config/initializers/constans.rb, at OPTIONS_SETTING for text-value of share_to_index
+      t.boolean :post_lounge
 
       t.timestamps
     end
