165dd32d9751c4863fef4fb44976f8957b881e9b
ubuntu11 <datnt@ubuntu.(none)>
2011-11-05 10:17:40 +0700

    create new topic with custom setting save to db

--- app/controllers/u_forums_controller.rb
+++ app/controllers/u_forums_controller.rb
@@ -21,12 +21,19 @@ class UForumsController < ApplicationController
     @ufo.user = current_user
     if @ufo.save
       flash[:notice] = "Your topic was successfully submitted."
+
+      custom_setting = UfoCustom.find_or_create_by_ufo_id(@ufo.id)
+      custom_setting.share_to_index = params[:ufo_setting]
+      custom_setting.post_lounge = params[:lounge_setting]
+      custom_setting.save
+
       @ufo = Ufo.new()
       render :action => "new"
     else
       flash[:notice] = "Failed to create new topic."
       render :action => "new"
     end
+
   end
 
   def dft_stgs
@@ -44,30 +51,12 @@ class UForumsController < ApplicationController
   end
 
   def save_custom
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "===="
-    puts "====a"
+    #THIS ACTION WILL BE APPLIED TO VIEW-DETAIL PAGE
     puts "==== shareto == #{params[:shareto]}"
     puts "==== lounge == #{params[:postlounge]}"
+
+    default_setting = UfoCustom.find_or_create_by_ufo_id(current_user.id)
+
 
     render :layout => false 
   end
