face177086f22ab5e9a3d8cd302c764e63bead13
ubuntu11 <datnt@ubuntu.(none)>
2011-11-08 14:53:08 +0700

    update

--- app/controllers/rate_cmts_controller.rb
+++ app/controllers/rate_cmts_controller.rb
@@ -1,15 +1,4 @@
 class RateCmtsController < ApplicationController
-  # def load_bk_like
-  #   @post = Post.find(params[:post_id])
-  #   @str_cur_page = params[:str_page_no] ? params[:str_page_no] : "1"
-  #   @rating_texts = @post.rating_texts.where(["rating=? and rated_type=?","1","#{params[:rated_type]}"]).order('created_at DESC').paginate(:page => @str_cur_page, :per_page => params[:str_perpage])
-  #   @perpage = params[:str_perpage]
-  #   @like_size = @post.rating_texts.where('rating="1"').size
-  #   @rated_type = params[:rated_type]
-  #   render :layout => false
-  # end
-
-
   def load_bk_like
 
     @str_cur_page = params[:str_page_no] ? params[:str_page_no] : "1"
@@ -46,25 +35,6 @@ class RateCmtsController < ApplicationController
     @rated_type = params[:rated_type]
     render :layout => false
   end
-
-
-  # def add_like_cmt
-  #   @post = Post.find(params[:post_id])
-  #   @cmt = RatingText.new()
-  #   @cmt.user = current_user
-  #   @cmt.post = @post
-  #   @cmt.rated_type = params[:rated_type]
-  #   @cmt.rating = 1
-
-  #   if ( @cmt.rated_type == "PostFood" || @cmt.rated_type == "PostMyx" || @cmt.rated_type == "PostParty" )
-  #     @cmt.rating = 2
-  #   end
-
-  #   @cmt.content = params[:cmt_cnt]
-  #   @cmt.save
-    
-  #   render :layout => false
-  # end
 
 
   def add_like_cmt
@@ -108,15 +78,6 @@ class RateCmtsController < ApplicationController
   end
 
 
-  # def load_bk_dislike
-  #   @post = Post.find(params[:post_id])
-  #   @str_cur_page = params[:str_page_no] ? params[:str_page_no] : "1"
-  #   @rating_texts = @post.rating_texts.where(["rating=? and rated_type=?","0","#{params[:rated_type]}"]).order('created_at DESC').paginate(:page => @str_cur_page, :per_page => params[:str_perpage])
-  #   @dislike_size = @post.rating_texts.where('rating = "0"').size
-  #   @rated_type = params[:rated_type]
-  #   @perpage = params[:str_perpage]
-  #   render :layout => false
-  # end
   def load_bk_dislike
     @str_cur_page = params[:str_page_no] ? params[:str_page_no] : "1"
     case params[:rated_type]
@@ -147,19 +108,6 @@ class RateCmtsController < ApplicationController
     render :layout => false
   end
 
-
-  # def add_dislike_cmt
-  #   @post = Post.find(params[:post_id])
-  #   @cmt = RatingText.new()
-  #   @cmt.user = current_user
-  #   @cmt.post = @post
-  #   @cmt.rated_type = params[:rated_type]
-  #   @cmt.rating = 0
-  #   @cmt.content = params[:cmt_dcnt]
-  #   @cmt.save
-    
-  #   render :layout => false
-  # end
 
   def add_dislike_cmt
     case params[:rated_type]
