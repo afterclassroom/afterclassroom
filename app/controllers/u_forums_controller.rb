c5ecbf5a5556a244b07bce215571580ef316fb3f
ubuntu11 <datnt@ubuntu.(none)>
2011-11-09 09:59:15 +0700

    paginate for comment

--- app/controllers/u_forums_controller.rb
+++ app/controllers/u_forums_controller.rb
@@ -9,6 +9,8 @@ class UForumsController < ApplicationController
 
   def show
     @ufo = Ufo.find(params[:id])
+    @ufo_cmts = @ufo.ufo_cmts.paginate(:page => params[:page], :per_page => 1)
+
     @ufo_cmt = UfoCmt.new()
   end
 
