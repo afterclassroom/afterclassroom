d817f73abc0f8d5cf7700783f20c3f9e74bcae0a
ubuntu11 <datnt@ubuntu.(none)>
2011-12-01 17:07:56 +0700

    update function send mail ufo

--- app/mailers/ufo_mail.rb
+++ app/mailers/ufo_mail.rb
@@ -2,17 +2,11 @@ class UfoMail < ActionMailer::Base
   default :from => "#{Setting.get(:support_name)} <#{Setting.get(:support_email)}>"
   default_url_options[:host] = "afterclassroom.com"
   
-  def testufo
-    mail :to => "datefield@yahoo.com", :subject => "test send mail"
-  end
-  
-  def inviteinform(arr_bcc,ufo_author)
-    test = []
-    test << "datefield@yahoo.com"
-    test << "ngothiendat@gmail.com"
-    @listbcc = arr_bcc
-    mail :to => ["datefield@yahoo.com","ngothiendat@gmail.com"], :subject => "#{ufo_author.name} invite you to join a topic."
-    
+  def inviteinform(email,ufo_author,friend,ufo)
+    @friend = friend
+    @ufo_author = ufo_author
+    @ufo = ufo
+    mail :to => email, :subject => "#{ufo_author.name} invite you to join a topic."
   end
   
 end
