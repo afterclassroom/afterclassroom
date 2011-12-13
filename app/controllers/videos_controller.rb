49975484a449f452558aee99993c16a04c239e18
ubuntu11 <datnt@ubuntu.(none)>
2011-12-13 11:16:48 +0700

    update

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -298,12 +298,12 @@ class VideosController < ApplicationController
                   TagVidMail.inform_creator_self_tag_success(@video,current_user).deliver
                   TagVidMail.inform_author_creator_self_tag_success(@video,current_user).deliver
                 when @video.user #case 2
-                  TagVidMail.inform_creator_tag_of_author_success(@video,current_user).deliver
-                  TagVidMail.inform_author_tag_of_author_success(@video,current_user).deliver
+#                  TagVidMail.inform_creator_tag_of_author_success(@video,current_user).deliver
+#                  TagVidMail.inform_author_tag_of_author_success(@video,current_user).deliver
                 else #another user #case 3
-                  TagVidMail.inform_creator_tag_of_user_success(@video,current_user,u).deliver
-                  TagVidMail.inform_author_tag_of_user_success(@video,current_user,u).deliver
-                  TagVidMail.inform_user_been_tagged(@video,current_user,u).deliver
+#                  TagVidMail.inform_creator_tag_of_user_success(@video,current_user,u).deliver
+#                  TagVidMail.inform_author_tag_of_user_success(@video,current_user,u).deliver
+#                  TagVidMail.inform_user_been_tagged(@video,current_user,u).deliver
                 end
               end              
             end
