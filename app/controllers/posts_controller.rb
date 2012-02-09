44240c6cc6b08cbfe65de3b5965a902a13b0c844
linkdigital <cms3@ubuntu.(none)>
2012-02-09 17:11:05 +0700

    Fix bugs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   #before_filter RubyCAS::Filter::GatewayFilter
   #before_filter RubyCAS::Filter, :except => [:report_abuse, :report_abuse_video, :create_report_abuse, :download]
   #before_filter :cas_user
-  before_filter :login_required, :except => [:report_abuse, :report_abuse_video, :create_report_abuse, :download]
+  before_filter :login_required, :except => [:report_abuse, :report_abuse_video, :create_report_abuse, :download, :comments_list, :view_all_comments]
 
   def create_comment
     comment = params[:comment]
